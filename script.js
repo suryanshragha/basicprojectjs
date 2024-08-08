fa6c503fc5f8c1f1cd7f2dd0e234e4e64c4c54f7
suryansh raghav <suryanshraghav222@gmail.com>
2024-08-08 19:52:54 +0530

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const form = document.querySelector('form')
-form.addEventListener('submit',function(e){
-    e.preventDefault();
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    
-    const results =  document.querySelector('#results');
-    if(height ===''|| height <0|| isNaN(height) ){
-      results.innerHTML = `please give a valid ${height}`    }
-      else if(weight ===''|| weight<0 || isNaN(weight)) {
-         results.innerHTML = `please give a valid ${weight}`
-      }
-      else {
-        const BMI = (weight/((height*height)/10000)).toFixed(2)
-        results.innerHTML =` <span>${BMI}</span>`
-      }
-    
-})
+const clock = document.getElementById('clock');
+
+setInterval(function(){
+ const date = new Date();
+ clock.innerHTML = date.toLocaleTimeString();
+},1000)
